da875baefd41108f9856eecc4c9b844cd649db0d
zoonny <hyungii@naver.com>
2023-03-11 11:51:13 +0900

    dat.GUI

--- canvas/index4.js
+++ canvas/index4.js
@@ -17,44 +17,16 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-const feGaussianBlur = document.querySelector("feGaussianBlur");
-const feColorMatrix = document.querySelector("feColorMatrix");
-
-const controls = new (function () {
-  this.blurValue = 40;
-  this.alphaChannel = 100;
-  this.alphaOffset = -23;
-})();
-
-let gui = new dat.GUI();
-gui.add(controls, "blurValue", 0, 100).onChange((value) => {
-  feGaussianBlur.setAttribute("stdDeviation", value);
-});
-gui.add(controls, "alphaChannel", 1, 200).onChange((value) => {
-  feColorMatrix.setAttribute(
-    "values",
-    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`
-  );
-});
-gui.add(controls, "alphaOffset", -40, 40).onChange((value) => {
-  feColorMatrix.setAttribute(
-    "values",
-    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`
-  );
-});
-
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.vy = vy;
-    // this.acc = 0.98;
-    this.acc = 1.03;
   }
 
   update() {
-    this.vy *= this.acc;
+    // this.y += 1;
     this.y += this.vy;
   }
 
--- canvas/index5.js
+++ canvas/index5.js
@@ -17,32 +17,6 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-const feGaussianBlur = document.querySelector("feGaussianBlur");
-const feColorMatrix = document.querySelector("feColorMatrix");
-
-const controls = new (function () {
-  this.blurValue = 40;
-  this.alphaChannel = 100;
-  this.alphaOffset = -23;
-})();
-
-let gui = new dat.GUI();
-gui.add(controls, "blurValue", 0, 100).onChange((value) => {
-  feGaussianBlur.setAttribute("stdDeviation", value);
-});
-gui.add(controls, "alphaChannel", 1, 200).onChange((value) => {
-  feColorMatrix.setAttribute(
-    "values",
-    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`
-  );
-});
-gui.add(controls, "alphaOffset", -40, 40).onChange((value) => {
-  feColorMatrix.setAttribute(
-    "values",
-    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`
-  );
-});
-
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x;
